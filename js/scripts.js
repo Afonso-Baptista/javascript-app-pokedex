85280a90ef07c71b6ebe9fa350c76c00a6cf65a9
Afonso Baptista <afonso.7.baptista@gmail.com>
2021-11-12 15:18:02 +0000

    Setting up project
    With html, css and js files & folders

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+/*
+Explanation:
+
+JS Rules:
+- Order of execution:
+    - Top to bottom!
+- Variable names:
+    - It must start with a letter
+    - It can only consist of letters, numbers, or underscores (_)
+    - It can’t include any special characters, such as ü or à
+    - Note: they're case-sensitive - e.g. myAGE and myAge are 2 different variables
+- Browser-supported JS edition:
+    - As of 11th November 2021: ES5 is the newest version that’s guaranteed to be supported by all active browsers!
+
+*/
+
+
+/*
+Explanation:
+
+JS Best Practices:
+- Writing JS Code:
+    - End every command with a semicolon ;
+    - Write each command on its own individual line
+    - Add blank lines between sections
+- Variable names:
+    - Use camelCase (e.g. myAge and not my_age)
+*/
+
+alert('Hello world');
+
+let favoriteFood = 'Steak';
+document.write(favoriteFood);
