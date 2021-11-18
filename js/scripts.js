384287d1cca4b173c26c16fe2ddfb686464f607a
Afonso Baptista <afonso.7.baptista@gmail.com>
2021-11-18 15:30:28 +0000

    Create 'pokemonList' array & add 4 objects
    To start off, add 4 pokémon objects with 5 key-value pairs each.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
 
+let pokemonList = [
+    {
+        name: 'Pikachu', 
+        height: 0.4, 
+        weight: 6, 
+        types: ['electric'], 
+        abilities: ['Static', 'Lightningrod']
+    },
+    {
+        name: 'Charmander', 
+        height: 0.6, 
+        weight: 8.5, 
+        types: ['fire'], 
+        abilities: ['Blaze', 'Solar-power']
+    },
+    {
+        name: 'Squirtle', 
+        height: 0.5, 
+        weight: 9, 
+        types: ['water'], 
+        abilities: ['Rain-dish', 'Torrent']
+    },
+    {
+        name: 'Bulbasaur', 
+        height: 0.7, 
+        weight: 6.9, 
+        types: ['grass', 'poison'], 
+        abilities: ['Chlorophyll', 'Overgrow']
+    }
+];
 
 
 
